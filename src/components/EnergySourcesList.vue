68af0da8f5cc9ff2a33bafba1aad7be496ef7e14
matapaska <marta.pasikowska@gmail.com>
2019-11-28 12:36:08 +0000

    Google charts added to component

--- src/components/EnergySourcesList.vue
+++ src/components/EnergySourcesList.vue
@@ -1,15 +1,39 @@
 <template lang="html">
   <div>
-    <ul>
-      <!-- <li v-for="(source, index) in energy-sources" >{{source.}}</li> -->
-    </ul>
+<GChart
+type = "ColumnChart"
+:data="chartData"
+:options="chartOptions"
+/>
   </div>
 </template>
 
 <script>
+import { GChart } from 'vue-google-charts'
 export default {
   name: "energy-sources-list",
-  props: ['energy-sources']
+  props: ['energy-sources'],
+  components: {
+    GChart
+  },
+  data () {
+    return {
+      // Array will be automatically processed with visualization.arrayToDataTable function
+      chartData: [
+        ['Year', 'Sales', 'Expenses', 'Profit'],
+        ['2014', 1000, 400, 200],
+        ['2015', 1170, 460, 250],
+        ['2016', 660, 1120, 300],
+        ['2017', 1030, 540, 350]
+      ],
+      chartOptions: {
+        chart: {
+          title: 'Company Performance',
+          subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+        }
+      }
+    }
+  }
 }
 </script>
 
